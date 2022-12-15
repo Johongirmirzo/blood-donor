dff89168511a6e9472b682e27d5978632b9f1209
Johongirr <johongir_rahimov@bk.ru>
2022-12-15 16:12:02 +0100

    update: disable access to protected routes

--- client/src/pages/BloodRequest/index.tsx
+++ client/src/pages/BloodRequest/index.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import type { RootState } from "../../redux/store";
 import HeroSection from "../../components/HeroSection";
@@ -10,6 +11,14 @@ const BloodRequests = () => {
   const heroImage = useSelector(
     (state: RootState) => state.contactPage.heroImage
   );
+  const navigate = useNavigate();
+  const authDonor = useSelector((state: RootState) => state.authUser);
+
+  useEffect(() => {
+    if (!authDonor.fullname) {
+      navigate("/login");
+    }
+  }, [authDonor]);
 
   return (
     <>
